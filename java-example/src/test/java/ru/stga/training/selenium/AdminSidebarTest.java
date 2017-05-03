886221a2848698183c3ad6df9e62919b8d1b84b9
sergeiden <sd100578@yandex.ru>
2017-05-03 19:13:46 +0300

    ДЗ.7 Переделан сценарий для разделов админки
    Переделан сценарий, проходящий по разделам админки

--- java-example/src/test/java/ru/stga/training/selenium/AdminSidebarTest.java
+++ java-example/src/test/java/ru/stga/training/selenium/AdminSidebarTest.java
@@ -5,9 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -22,7 +24,7 @@ public class AdminSidebarTest {
   public void start() {
     driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    wait = new WebDriverWait(driver, 10);
+    wait = new WebDriverWait(driver, 5);
 
   }
 
@@ -35,140 +37,26 @@ public class AdminSidebarTest {
     driver.findElement(By.cssSelector(".btn[name=login]")).click();
     driver.findElement(By.id("search"));
 
-    driver.findElement(By.cssSelector("#box-apps-menu #app-:nth-child(1)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-template")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-logotype")).click();
-    driver.findElement(By.tagName("h1"));
+    int index = 0;
+    int sidebarMenuElementsCount = driver.findElements(By.cssSelector("#app-")).size();
+    while (index < sidebarMenuElementsCount) {
+      List<WebElement> sidebarMenuElements = driver.findElements(By.cssSelector("#app-"));
+      sidebarMenuElements.get(index).click();
+      driver.findElement(By.cssSelector("h1"));
+      index++;
 
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(2)")).click();
-    driver.findElement(By.cssSelector("#doc-product_groups")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-catalog")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-option_groups")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-manufacturers")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-suppliers")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-delivery_statuses")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-sold_out_statuses")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-quantity_units")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-csv")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(3)")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(4)")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(5)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-csv")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-customers")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-newsletter")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(6)")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(7)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-storage_encoding")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-languages")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(8)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-customer")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-shipping")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-payment")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-order")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-order_total")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-jobs")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(9)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-order_statuses")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-orders")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(10)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-csv")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-pages")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(11)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-monthly_sales")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-most_sold_products")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-most_shopping_customers")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(12)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-store_info")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-defaults")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-general")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-listings")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-images")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-checkout")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-advanced")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-security")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(13)")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(14)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-tax_rates")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-tax_classes")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(15)")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-search")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-scan")).click();
-    driver.findElement(By.tagName("h1"));
-    driver.findElement(By.cssSelector("#doc-csv")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(16)")).click();
-    driver.findElement(By.tagName("h1"));
-
-    driver.findElement(By.cssSelector(".list-vertical #app-:nth-child(17)")).click();
-    driver.findElement(By.tagName("h1"));
-
+      List<WebElement> submenuElements = driver.findElements(By.cssSelector(".docs span.name"));
+      int submenuElementsCount = submenuElements.size();
+      if (submenuElementsCount > 0) {
+        int subIndex = 0;
+        while (subIndex < submenuElementsCount) {
+          submenuElements = driver.findElements(By.cssSelector(".docs span.name"));
+          submenuElements.get(subIndex).click();
+          driver.findElement(By.tagName("h1"));
+          subIndex++;
+        }
+      }
+    }
   }
 
   @After
