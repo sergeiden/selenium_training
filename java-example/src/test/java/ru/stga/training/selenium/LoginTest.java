ede36fbf71482a5ee8c01fb256866b7680d52185
sergeiden <sd100578@yandex.ru>
2017-05-03 13:55:27 +0300

    Добавлен тест для проверки панели администратора
    Добавлен AdminSideBarTest

--- java-example/src/test/java/ru/stga/training/selenium/LoginTest.java
+++ java-example/src/test/java/ru/stga/training/selenium/LoginTest.java
@@ -4,28 +4,35 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 /**
  * Created by serg on 19.04.2017.
  */
 public class LoginTest {
   private WebDriver driver;
+
   private WebDriverWait wait;
 
   @Before
   public void start() {
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    FirefoxOptions options = new FirefoxOptions().setLegacy(false);
+//    options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")));
+    options.setBinary(new FirefoxBinary(new File("d:\\Tools\\FFNightly\\firefox.exe")));
+
+    driver = new FirefoxDriver(options);
+    System.out.println(((HasCapabilities) driver).getCapabilities());
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wait = new WebDriverWait(driver, 10);
+
   }
 
   @Test
--- java-example/src/test/java/ru/stga/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stga/training/selenium/MyFirstTest.java
@@ -4,21 +4,30 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
 
   private WebDriver driver;
+
   private WebDriverWait wait;
 
   @Before
     public void start() {
-    driver = new FirefoxDriver();
+    FirefoxOptions options = new FirefoxOptions().setLegacy(true);
+//    options.setBinary(new FirefoxBinary(new File("d:\\Tools\\FFNightly\\firefox.exe")));
+    options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")));
+    driver = new FirefoxDriver(options);
+    System.out.println(((HasCapabilities) driver).getCapabilities());
     wait = new WebDriverWait(driver, 10);
   }
 
